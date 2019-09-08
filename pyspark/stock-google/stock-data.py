d108319a29949abf1fc793d13034276b47b01c91
Jason Feng <q15928@hotmail.com>
2019-09-08 14:56:24 +1000

    updated

--- pyspark/stock-google/stock-data.py
+++ pyspark/stock-google/stock-data.py
@@ -16,23 +16,25 @@ df = spark.read.format("csv") \
 # in case we need to convert the Date column to timestamp explicitly     
 # df = df.withColumn("Date", F.unix_timestamp("Date", "yyyy-MM-dd").cast("timestamp"))
 
-df = df.withColumn("Id", F.lit("GOOG"))
+df = df.withColumn("Symbol", F.lit("GOOG"))
 df.printSchema()
 tmp_df1 = df.where("Date < '2018-08-01'")
 
 df.createOrReplaceTempView("df")
 
 # get previous row (lag) and next row (lead)
-win_spec = Window.partitionBy(F.col("Id")). orderBy(F.col("Date").asc())
+win_spec = Window.partitionBy(F.col("Symbol")). orderBy(F.col("Date").asc())
 tmp_df = df.withColumn("close_lag_1", F.lag(F.col("Close"), 1).over(win_spec)) \
     .withColumn("close_lead_1", F.lead(F.col("Close"), 1).over(win_spec))
 tmp_df.show()
 
 # get rolling average of last 7 days with PySpark
+# ref https://stackoverflow.com/questions/33207164/spark-window-functions-rangebetween-dates
+# ref https://stackoverflow.com/questions/45806194/pyspark-rolling-average-using-timeseries-data
 # a lambda function to convert days into seconds
 days = lambda d: d * 24 * 60 * 60
 
-win_spec = Window.partitionBy(F.col("Id")) \
+win_spec = Window.partitionBy(F.col("Symbol")) \
     .orderBy(F.col("Date").cast("long")) \
     .rangeBetween(-days(7), 0)
 col_mean_7d = F.avg("Close").over(win_spec)
@@ -50,8 +52,28 @@ tmp_df = spark.sql(stmt)
 tmp_df.show()
 
 
+
 df.where("Date >= '2018-07-20' and Date <= '2018-07-28'") \
     .select(F.avg("Close")) \
     .show()
 
-    
+
+
+df = spark.createDataFrame([(17, "2017-03-10T15:27:18+00:00"),
+                        (13, "2017-03-15T12:27:18+00:00"),
+                        (19, "2017-03-15T02:27:18+00:00"),
+                        (25, "2017-03-18T11:27:18+00:00")],
+                        ["Close", "timestampGMT"])
+df = df.withColumn('Symbol', F.lit('GOOG'))
+df = df.withColumn('timestampGMT', df.timestampGMT.cast('timestamp'))
+df = df.withColumn('Date', F.to_date('timestampGMT').cast('timestamp'))
+
+
+days = lambda d: d * 24 * 60 * 60
+
+win_spec = Window.partitionBy(F.col("Symbol")) \
+    .orderBy(F.col("Date").cast("long")) \
+    .rangeBetween(-days(7), 0)
+col_mean_7d = F.avg("Close").over(win_spec)
+tmp_df = df.withColumn("mean_7d", col_mean_7d)
+tmp_df.show()
